9fb05928ee8deaf762dd754779ba29dcf6c5a3d3
Akshay Patel <akshaypatel@Akshays-MacBook-Air.local>
2022-10-16 14:05:28 -0700

    Commit 2pm Sunday

--- src/pages/ProfileScreen/index.js
+++ src/pages/ProfileScreen/index.js
@@ -1,14 +1,67 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 
 const ProfileScreen = () => {
   return (
     <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
-      <Text>ProfileScreen</Text>
+      <Text style={styles.profileName}>Hello, Brenda!</Text>
+      <Text style={styles.statistics}>Your Statistics</Text>
+      <Text style={styles.statisticsInfo}>27 Plans Made</Text>
+      <Text style={styles.statisticsInfo}>250 Liked Playlists</Text>
+      <Text style={styles.preferences}>Preferences</Text>
+      <Text style={styles.preference}>Food {"&"} Drinks</Text>
+      <Text style={styles.preference}>Things to Do</Text>
+      <Text style={styles.preference}>Shopping</Text>
+      <TouchableOpacity
+        style={{
+          alignSelf: "center",
+          backgroundColor: "#2DB493",
+          paddingVertical: 15,
+          paddingHorizontal: 75,
+          borderRadius: 5,
+          marginTop: 200,
+        }}
+      >
+        <Text style={{ fontSize: 17, color: "white" }}>Explore</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
 export default ProfileScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  profileName: {
+    marginLeft: 25,
+    marginTop: 15,
+    fontSize: 19,
+  },
+  statistics: {
+    fontWeight: "bold",
+    marginTop: 50,
+    fontSize: 18,
+    marginLeft: 25,
+  },
+  statisticsInfo: {
+    marginLeft: 25,
+    marginTop: 15,
+  },
+  preferences: {
+    fontWeight: "bold",
+    marginTop: 50,
+    fontSize: 18,
+    marginLeft: 25,
+  },
+  preference: {
+    marginLeft: 25,
+    marginTop: 15,
+    marginBottom: 30,
+    fontSize: 15,
+  },
+});
